1e4e13b36bc73ac27def44a0399e5c4e9cf8b842
Carlos Escamilla <carlosaescamilla3@gmail.com>
2026-06-09 21:47:47 -0700

    Add: maximum-difference-between-even-and-odd-frequency-i - submission-5

---
+++ Data Structures & Algorithms/maximum-difference-between-even-and-odd-frequency-i/submission-5.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maxDifference(self, s: str) -> int:
+        char_count = {}
+        even_min_freq = float("inf")
+        odd_max_freq = float("-inf")
+        res = float('-inf')
+
+        for char in s:
+            if char not in char_count:
+                char_count[char] = 0
+            char_count[char] += 1
+
+        for char in char_count:
+            if char_count[char] % 2 == 0:
+                even_min_freq = min(even_min_freq, char_count[char])
+            else:
+                odd_max_freq = max(odd_max_freq, char_count[char])
+
+            if even_min_freq != float("inf") and odd_max_freq != float("-inf"):
+                diff = odd_max_freq - even_min_freq
+                res = max(diff, res)
+
+        return res
